662a364da72e74c7ec1285ec20b51607db5f306a
Paul Nel <gpaul.nel@gmail.com>
2019-07-21 16:45:49 +0200

    updted with pie chart

--- docs/scripts/scripts.js
+++ docs/scripts/scripts.js
@@ -1,14 +1,34 @@
 async function init() {
     var data = await d3.csv("courses.csv");
-    var duration = []
-    var course = []
+    var duration = [];
+    var course = [];
     data.forEach(row => {
-        course.push(row.Course)
-        duration.push(row.Total_Time)
+        course.push(row.Course);
+        duration.push(row.Total_Time);
     });
-    console.log("test2")
-    console.log(course[1])
-    console.log(duration[1])
-    var pie = d3.pie()
-    console.log(pie(duration))
+    console.log("test3")
+
+    var svg = d3.select("svg"),
+        width = svg.attr("widht"),
+        height = svg.attr("height"),
+        radius = Math.min(width, height) / 2,
+        g = svg.append("g").attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+    var color = d3.scaleOrdinal(['#4daf4a', '#377eb8', '#ff7f00', '#984ea3', '#e41a1c']);
+
+    var pie = d3.pie();
+
+    var arc = d3.arc()
+        .innerRadius(0)
+        .outerRadius(radius);
+
+    var arcs = g.selectAll("arc")
+        .data(pie(duration))
+        .enter()
+        .append(g)
+        .attr("class", "arc");
+
+    arcs.append("path")
+        .attr("fill", (d, i) => { return color(i) })
+        .attr("d", arc);
 }
