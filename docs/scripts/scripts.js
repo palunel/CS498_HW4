3f6d3e0149ccc43e362f527ced9a05fe92ffc5e9
Paul Nel <gpaul.nel@gmail.com>
2019-07-21 18:05:47 +0200

    update

--- docs/scripts/scripts.js
+++ docs/scripts/scripts.js
@@ -6,7 +6,7 @@ async function init() {
         course.push(row.Course);
         duration.push(row.Total_Time);
     });
-    console.log("test18")
+    console.log("test19")
     console.log(duration)
 
     var tooltip = d3.select("#tooltip")
@@ -30,8 +30,8 @@ async function init() {
 
     function showTooltip(event, tipText) {
         tooltip.style("opacity", 1)
-            .style("left", (d3.event.pageX) + "px")
-            .style("top", (d3.event.pageY) + "px")
+            .style("left", (event.pageX) + "px")
+            .style("top", (event.pageY) + "px")
             .html(tipText)
     }
 
