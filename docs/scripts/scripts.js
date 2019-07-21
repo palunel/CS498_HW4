00921ea68b55ea4a5ea9b4d16ed7e45bc30bb507
Paul Nel <gpaul.nel@gmail.com>
2019-07-21 16:51:33 +0200

    update

--- docs/scripts/scripts.js
+++ docs/scripts/scripts.js
@@ -6,29 +6,36 @@ async function init() {
         course.push(row.Course);
         duration.push(row.Total_Time);
     });
-    console.log("test3")
+    console.log("test4")
+    console.log(duration)
 
     var svg = d3.select("svg"),
-        width = svg.attr("widht"),
+        width = svg.attr("width"),
         height = svg.attr("height"),
         radius = Math.min(width, height) / 2,
         g = svg.append("g").attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
     var color = d3.scaleOrdinal(['#4daf4a', '#377eb8', '#ff7f00', '#984ea3', '#e41a1c']);
 
+    // Generate the pie
     var pie = d3.pie();
 
+    // Generate the arcs
     var arc = d3.arc()
         .innerRadius(0)
         .outerRadius(radius);
 
+    //Generate groups
     var arcs = g.selectAll("arc")
         .data(pie(duration))
         .enter()
-        .append(g)
-        .attr("class", "arc");
+        .append("g")
+        .attr("class", "arc")
 
+    //Draw arc paths
     arcs.append("path")
-        .attr("fill", (d, i) => { return color(i) })
+        .attr("fill", function (d, i) {
+            return color(i);
+        })
         .attr("d", arc);
 }
