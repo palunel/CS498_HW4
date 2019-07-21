e4edf233503e63b1d7d7600ae276ea9909df7b57
Paul Nel <gpaul.nel@gmail.com>
2019-07-21 15:21:58 +0200

    Updated scripts

--- docs/scripts/scripts.js
+++ docs/scripts/scripts.js
@@ -1,26 +1,5 @@
 async function init() {
-    data = await d3.csv("https://flunky.github.io/cars2017.csv");
-    xdomain = [10, 150];
-    xrange = [0, 400];
-    ydomain = [10, 150];
-    yrange = [400, 0];
-    xs = d3.scaleLog().domain(xdomain).range(xrange);
-    ys = d3.scaleLog().domain(ydomain).range(yrange);
+    var data = await d3.csv("docs/data/courses.csv");
+    console.log(data)
 
-    d3.select("svg").append("g").attr("transform", "translate(50,50)")
-        .selectAll("circle")
-        .data(data)
-        .enter()
-        .append("circle")
-        .attr("cx", (d) => { return xs(d.AverageCityMPG) })
-        .attr("cy", (d) => { return ys(d.AverageHighwayMPG) })
-        .attr("r", (d) => { return 2 + parseInt(d.EngineCylinders) });
-
-    d3.select("svg").append("g")
-        .attr("transform", "translate(50,50)")
-        .call(d3.axisLeft(ys).tickValues([10, 20, 50, 100]).tickFormat(d3.format("~s")));
-
-    d3.select("svg").append("g")
-        .attr("transform", "translate(50,450)")
-        .call(d3.axisBottom(xs).tickValues([10, 20, 50, 100]).tickFormat(d3.format("~s")));
 }
