9f200ef3f02415391f7ad5205d62da24935ea98d
Paul Nel <gpaul.nel@gmail.com>
2019-07-21 16:37:03 +0200

    updates

--- docs/scripts/scripts.js
+++ docs/scripts/scripts.js
@@ -1,7 +1,14 @@
 async function init() {
     var data = await d3.csv("courses.csv");
-    console.log("test1")
-    console.log(data[1].Total_Time)
+    var duration = []
+    var course = []
+    data.forEach(row => {
+        course.push(row.Course)
+        duration.push(row.Total_Time)
+    });
+    console.log("test2")
+    console.log(course[1])
+    console.log(duration[1])
     var pie = d3.pie()
-    console.log(pie(data.Total_Time))
+    console.log(pie(duration))
 }
