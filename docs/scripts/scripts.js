31b1761579b89782cf666d01647af48659fe80b8
Paul Nel <gpaul.nel@gmail.com>
2019-07-21 16:03:02 +0200

    Updated courses

--- docs/scripts/scripts.js
+++ docs/scripts/scripts.js
@@ -1,8 +1,7 @@
 async function init() {
-    var data = await d3.csv("courses.csv");
+    var data = await d3.csv("../data/courses.csv");
     // console.log(data)
     var pie = d3.pie()
-    pie(data)
     console.log(pie(data))
 
 
