391110f541266ef392f79610f0b23bfd3063318b
Paul Nel <gpaul.nel@gmail.com>
2019-07-21 15:30:48 +0200

    updates

--- docs/scripts/scripts.js
+++ docs/scripts/scripts.js
@@ -1,5 +1,4 @@
 async function init() {
-    var data = await d3.csv("docs/data/courses.csv");
-    console.log(data)
+    var data = await d3.csv("https://github.com/palunel/CS498_HW4/blob/master/docs/data/courses.csv");
 
 }
