6ac709f8adbf1bd37a78257a59635221d7b82b1c
Paul Nel <gpaul.nel@gmail.com>
2019-07-24 19:18:41 +0200

    updated with times per day chart

--- docs/scripts/scripts.js
+++ docs/scripts/scripts.js
@@ -82,7 +82,9 @@ async function courseOverview() {
 async function courseDetail(course) {
     var width = d3.select(".course").attr("width");
     var height = d3.select(".course").attr("height");
-    var data = await d3.csv("course_tasks1.csv");
+    var verticalMargin = 90;
+    var horisontalMargin = 100;
+    var data = await d3.csv("course_tasks.csv");
     var tasks = [];
     var duration = [];
     var tooltip2 = d3.select("#tooltip2");
@@ -99,10 +101,10 @@ async function courseDetail(course) {
 
     console.log(course, maxValue);
 
-    var scale = ((height - 100) / maxValue);
+    var scale = ((height - 2 * verticalMargin) / maxValue);
 
     console.log(scale);
-    var interval = (width - 200) / (tasks.length);
+    var interval = (width - 2 * horisontalMargin) / (tasks.length);
     document.getElementById("title_1").innerHTML = course;
 
     d3.select(".course")
@@ -114,8 +116,8 @@ async function courseDetail(course) {
         .style("stroke", "#ffffff")
         .attr("width", interval)
         .attr("height", (d) => { return scale * d })
-        .attr("x", (d, i) => { return (interval * (i) + 50) })
-        .attr("y", (d) => { return (height - 50) - scale * d })
+        .attr("x", (d, i) => { return (interval * (i) + horisontalMargin) })
+        .attr("y", (d) => { return (height - verticalMargin) - scale * d })
         .on("mouseover", (d, i) => {
             tooltip2.style("opacity", 1)
                 .style("left", (d3.event.pageX) + "px")
@@ -138,24 +140,182 @@ async function courseDetail(course) {
 
     var y = d3.scaleLinear()
         .domain([0, maxValue])
-        .range([height - 100, 0]);
+        .range([height - 2 * verticalMargin, 0]);
 
 
     d3.select(".course")
         .append("g")
-        .attr("transform", "translate(50,50)")
+        .attr("transform", "translate(" + horisontalMargin + "," + verticalMargin + ")")
         .call(d3.axisLeft(y));
 
     console.log(tasks)
     var x = d3.scaleBand()
         .domain(tasks)
-        .range([0, width - 200])
+        .range([0, width - 2 * horisontalMargin]);
+
+    var xTranslate = '' + (parseInt(height) - verticalMargin);
+    d3.select(".course")
+        .append("g")
+        .attr("transform", "translate(" + horisontalMargin + "," + xTranslate + ")")
+        .call(d3.axisBottom(x))
+        .selectAll("text")
+        .attr("transform", "translate(-10,10)rotate(-45)")
+        .style("text-anchor", "end");
+}
+
+async function daysOverview() {
+    var width = d3.select(".weekday").attr("width");
+    var height = d3.select(".weekday").attr("height");
+    var verticalMargin = 90;
+    var horisontalMargin = 100;
+    var data = await d3.csv("course_days.csv");
+    var tooltip2 = d3.select("#tooltip2");
+    var days = [];
+    var totalDays = [];
+    data.forEach(row => {
+        days.push(row.Day);
+        totalDays.push(row.Total);
+    });
+    var maxValue = 0;
+    totalDays.forEach(item => {
+        if (parseFloat(item) > maxValue) {
+            maxValue = (parseFloat(item));
+        }
+    });
+    var scale = ((height - 2 * verticalMargin) / maxValue);
+    var interval = (width - 2 * horisontalMargin) / (days.length);
+    document.getElementById("title_2").innerHTML = "Total time per day of the week";
+
+    d3.select(".weekday")
+        .selectAll("rect")
+        .data(totalDays)
+        .enter()
+        .append("rect")
+        .attr("fill", "#5355ae")
+        .style("stroke", "#ffffff")
+        .attr("width", interval)
+        .attr("height", (d) => { return scale * d })
+        .attr("x", (d, i) => { return (interval * (i) + horisontalMargin) })
+        .attr("y", (d) => { return (height - verticalMargin) - scale * d })
+        .on("mouseover", (d, i) => {
+            tooltip2.style("opacity", 1)
+                .style("left", (d3.event.pageX) + "px")
+                .style("top", (d3.event.pageY) + "px")
+                .html(parseInt(totalDays[i]).toFixed(0) + " hours");
+        })
+        .on("mouseout", (d, i) => {
+            tooltip2
+                .style("opacity", 0)
+                .style("left", 0)
+                .style("top", 0)
+        })
+        .on("click", (d, i) => {
+            tooltip2
+                .style("opacity", 0)
+                .style("left", 0)
+                .style("top", 0);
+            loadCourseOverview();
+        });
+
+    var y = d3.scaleLinear()
+        .domain([0, maxValue])
+        .range([height - 2 * verticalMargin, 0]);
+
+
+    d3.select(".weekday")
+        .append("g")
+        .attr("transform", "translate(" + horisontalMargin + "," + verticalMargin + ")")
+        .call(d3.axisLeft(y));
+
+    var x = d3.scaleBand()
+        .domain(days)
+        .range([0, width - 2 * horisontalMargin]);
+
+    var xTranslate = '' + (parseInt(height) - verticalMargin);
+    d3.select(".weekday")
+        .append("g")
+        .attr("transform", "translate(" + horisontalMargin + "," + xTranslate + ")")
+        .call(d3.axisBottom(x))
+        .selectAll("text")
+        .attr("transform", "translate(-10,10)rotate(-45)")
+        .style("text-anchor", "end");
+}
+async function daysOverview1() {
+    var width = d3.select(".weekday").attr("width");
+    var height = d3.select(".weekday").attr("height");
+    var data = await d3.csv("course_days.csv");
+    var days = [];
+    var totalDays = [];
+    var duration = [];
+    var tooltip2 = d3.select("#tooltip2");
+    data.forEach(row => {
+        days.push(row.Day);
+        totalDays.push(row.Total);
+    });
+    var maxValue = 0;
+    totalDays.forEach(item => {
+        if (parseFloat(item) > maxValue) {
+            maxValue = (parseFloat(item));
+        }
+    });
+    var interval = (height - 200) / (totalDays.length);
+    var scale = ((width - 100) / maxValue);
+    console.log(days, totalDays);
+
+    document.getElementById("title_2").innerHTML = "Days of the week";
+
+    d3.select(".weekday")
+        .selectAll("rect")
+        .data(totalDays)
+        .enter()
+        .append("rect")
+        .attr("fill", "#5355ae")
+        .style("stroke", "#ffffff")
+        .attr("height", interval)
+        .attr("width", (d) => { return scale * d })
+        .attr("y", (d, i) => { return (interval * (i) + 50) })
+        .attr("x", "50")
+        .on("mouseover", (d, i) => {
+            tooltip2.style("opacity", 1)
+                .style("left", (d3.event.pageX) + "px")
+                .style("top", (d3.event.pageY) + "px")
+                .html(parseInt(totalDays[i]).toFixed(0) + " hours");
+        })
+        .on("mouseout", (d, i) => {
+            tooltip2
+                .style("opacity", 0)
+                .style("left", 0)
+                .style("top", 0)
+        })
+        .on("click", (d, i) => {
+            tooltip2
+                .style("opacity", 0)
+                .style("left", 0)
+                .style("top", 0)
+            // loadCourseOverview();
+
+        })
+        .append("a")
+        .attr("display", "inline-block")
+        .attr("innerHtml", (i) => { return days[i] });
+
+    var y = d3.scaleBand()
+        .domain(days)
+        .range([0, height - 200])
+    d3.select(".weekday")
+        .append("g")
+        .attr("transform", "translate(50,50)")
+        .call(d3.axisLeft(y));
+
+    var x = d3.scaleLinear()
+        .domain([0, maxValue])
+        .range([0, width - 100]);
 
     console.log("translate(50," + (parseInt(height) + 50) + ")")
 
-    d3.select(".course")
+    d3.select(".weekday")
         .append("g")
-        .attr("transform", "translate(50,550)")
+        .attr("transform", "translate(50,450)")
         .call(d3.axisBottom(x));
 }
 
@@ -173,5 +333,6 @@ async function loadCourseOverview() {
 
 async function init() {
     await courseOverview();
+    await daysOverview();
     console.log("test54");
 }
