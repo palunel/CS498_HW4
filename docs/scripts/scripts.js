19eaec7253661679c0aaa4008adb774d6a16e26d
Paul Nel <gpaul.nel@gmail.com>
2019-07-24 08:18:19 +0200

    update

--- docs/scripts/scripts.js
+++ docs/scripts/scripts.js
@@ -12,9 +12,9 @@ async function courseOverview() {
         total += parseInt(row.Total_Time);
     });
 
-    var tooltip = d3.select("#tooltip");
+    var tooltip1 = d3.select("#tooltip1");
 
-    var svg = d3.select("svg#course"),
+    var svg = d3.select(".course"),
         width = svg.attr("width"),
         height = svg.attr("height"),
         radius = 0.8 * Math.min(width, height) / 2,
@@ -35,6 +35,8 @@ async function courseOverview() {
         .innerRadius(0)
         .outerRadius(1.2 * radius);
 
+    document.getElementById("title_1").innerHTML = "Distribution of time spent per subject";
+
     //Generate groups
     g.selectAll("path")
         .data(pie(duration))
@@ -43,24 +45,25 @@ async function courseOverview() {
         .attr("d", arc)
         .attr("fill", (d, i) => { return color(i) })
         .on("mouseover", (d, i) => {
-            tooltip.style("opacity", 1)
+            tooltip1.style("opacity", 1)
                 .style("left", (d3.event.pageX) + "px")
                 .style("top", (d3.event.pageY) + "px")
                 .html("<b>" + course[i] + "</b>" + "<br> Total time: " + parseInt(duration[i]).toFixed(0) + " hours");
         })
         .on("mouseout", (d, i) => {
-            tooltip
+            tooltip1
                 .style("opacity", 0)
                 .style("left", 0)
                 .style("top", 0)
         })
-        .on("click", () => {
-            tooltip
+        .on("click", (d, i) => {
+            tooltip1
                 .style("opacity", 0)
                 .style("left", 0)
                 .style("top", 0);
-            removeCourseOverview();
+            loadCourseDetail(course[i]);
         });
+
     g.selectAll("text")
         .data(pie(duration))
         .enter()
@@ -76,24 +79,97 @@ async function courseOverview() {
         .style("font-weight", "bold")
 }
 
-async function courseDetail() {
-    var data = await d3.csv("courses_tasks.csv");
+async function courseDetail(course) {
+    var width = d3.select(".course").attr("width");
+    var height = d3.select(".course").attr("height");
+    var data = await d3.csv("course_tasks.csv");
+    var tasks = [];
     var duration = [];
-    var course = [];
-    var codes = [];
+    var tooltip2 = d3.select("#tooltip2");
+    data.forEach(row => {
+        tasks.push(row.Task);
+        duration.push(row[course])
+    });
+    var maxValue = 0;
+    duration.forEach(item => {
+        if (parseFloat(item) > maxValue) {
+            maxValue = (parseFloat(item));
+        }
+    });
 
+    var scale = ((height - 100) / maxValue);
+
+    console.log(scale);
+    var interval = (width - 200) / (tasks.length);
+    document.getElementById("title_1").innerHTML = course;
+
+    d3.select(".course")
+        .selectAll("rect")
+        .data(duration)
+        .enter()
+        .append("rect")
+        .attr("fill", "#5355ae")
+        .style("stroke", "#ffffff")
+        .attr("width", interval)
+        .attr("height", (d) => { return scale * d })
+        .attr("x", (d, i) => { return (interval * (i) + 50) })
+        .attr("y", (d) => { return (height - 50) - scale * d })
+        .on("mouseover", (d, i) => {
+            tooltip2.style("opacity", 1)
+                .style("left", (d3.event.pageX) + "px")
+                .style("top", (d3.event.pageY) + "px")
+                .html(parseInt(duration[i]).toFixed(0) + " hours");
+        })
+        .on("mouseout", (d, i) => {
+            tooltip2
+                .style("opacity", 0)
+                .style("left", 0)
+                .style("top", 0)
+        })
+        .on("click", (d, i) => {
+            tooltip2
+                .style("opacity", 0)
+                .style("left", 0)
+                .style("top", 0);
+            loadCourseOverview();
+        });
+
+    var y = d3.scaleLinear()
+        .domain([0, maxValue])
+        .range([height - 100, 0]);
+
+
+    d3.select(".course")
+        .append("g")
+        .attr("transform", "translate(50,50)")
+        .call(d3.axisLeft(y));
+
+    console.log(tasks)
+    var x = d3.scaleBand()
+        .domain(tasks)
+        .range([0, width - 200])
+
+    console.log("translate(50," + (parseInt(height) + 50) + ")")
+
+    d3.select(".course")
+        .append("g")
+        .attr("transform", "translate(50,550)")
+        .call(d3.axisBottom(x));
 }
 
-async function removeCourseOverview() {
-    console.log('called clear')
-    var svg = d3.select("svg#course")
+async function loadCourseDetail(course) {
+    var svg = d3.select(".course")
     svg.selectAll("*").remove();
-    await courseDetail();
-    console.log("detail rendered")
+    await courseDetail(course);
+}
+async function loadCourseOverview() {
+    var svg = d3.select(".course")
+    svg.selectAll("*").remove();
+    await courseOverview();
 }
 
 
 async function init() {
     await courseOverview();
-    console.log("test53");
+    console.log("test54");
 }
